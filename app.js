beed0dea05314de4224f1aa121f6f5dd5e518578
htevin <tevinhamilton@yahoo.com>
2022-08-23 12:39:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//let count = 1;
+
+//while (count <= 100) {
+//    console.log(count)
+//    count = count +1
+//    }
+
+
+//   for (let i = 1; i <= 20; i = i+1){
+  // if (i % 3 ===0 && i % 5 ===0)
+  // console.log (`${i} --> Frontend Simplified`)
+
+ //  else if(i % 3 ===0)
+  // console.log (`${i} --> Frontend`)
+
+  // else if(i % 5 ===0)
+  // console.log (`${i} --> Simplified`)
+
+ //  else console.log (`${i} --> ${i}`)
+// }
+
+let str = "Frontend Simplified"
+
+for (let i=0; i<str.length; i= i+1)
+{
+    console.log(str[i])
+}
